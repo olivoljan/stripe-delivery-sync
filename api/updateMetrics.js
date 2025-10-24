60d4828c0b8dde4d606d391c99c3390a6b425ff4
olivoljan <olivkassen@gmail.com>
2025-10-24 23:43:58 +0200

    Update updateMetrics.js range to Data sheet

--- api/updateMetrics.js
+++ api/updateMetrics.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
 
     await sheets.spreadsheets.values.append({
       spreadsheetId: process.env.GOOGLE_SHEET_ID,
-      range: 'Data!A:D',
+      range: 'Data',
       valueInputOption: 'USER_ENTERED',
       requestBody: {
         values: [[new Date().toISOString().split('T')[0], planned, delivered, canceled]],
